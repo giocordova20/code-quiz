f966c47749b2c97ac8eec5e6cfc9c178caf28db9
giocode <giocordova@gmail.com>
2020-09-11 21:44:53 -0400

    added new code for the timer and added logic to stop it at 0 if the penalty drops it below 0. Added color formating to the timer count down button based on the time left.

--- script.js
+++ script.js
@@ -1,65 +1,122 @@
-var timerEl = document.querySelector('#timer'); // Displays the time left on the button
+var timerButton = document.querySelector('#timer'); // Displays the time left on the button
+var timer = 10;     // Start Time
+var past = 0;       // Counter for the time that has elapsed
+var timerLeft = 0;  // Variable to track timer-past
+var interval;       
+
+///////////////////////
+//   Answer buttons  //
+///////////////////////
+var 
+
+
+////////////////////////////
+//  Code Quiz Questions   //
+////////////////////////////
+
+// var questions = [
+//     {
+//         title: "Alternate text is required for images in case the image does not render. Which of the following is the correct?",
+//         choices: ["<img src="indy500.png" alt="Indy 500 Race Car">", "<img src="indy500.png" altt="Indy 500 Race Car">", "<img src="indy500.png" alternate="Indy 500 Race Car">", "<img src="indy500.png" alt=="Indy 500 Race Car">"],
+//         answer: "<img src="indy500.png" alt="Indy 500 Race Car">"
+//     },
+//     {
+//         title: "<a> tags are used to make text a link. Which of the following is correct?",
+//         choices: ["<a href = "https://google.com">Google Search<a>", "<a href = "https://google.com">Google Search</a>", "</a href = "https://google.com">Google Search</a>", "<a> href = "https://google.com">Google Search</a>"],
+//         answer: "<a href = "https://google.com">Google Search</a>"
+//     },        
+//      ];
+
+// questions[i].title
+// questions[i.].answer
+// questions[i].choices[x]
 
 
 
-var poem = "Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.";
-var words = poem.split(" ");
+console.log("timer: " , timer);
 
-var mainEl = document.getElementById("main");
-//var timerEl = document.getElementById("countdown");
-var bodyEl = document.createElement("div");
-
-var i = 0;
-
-// var millisecondsPerWord = prompt("How many milliseconds between words would you like?");
-
-function countdown() {
-    var timeLeft = 5;
-  
-    var timeInterval = setInterval(function() {
-      timerEl.textContent = "Time Left: "+ timeLeft + "s";
-      timeLeft--;
-        console.log("Timer left")
-      if (timeLeft === 0) {
-        timerEl.textContent = "Time Left: 0";
-/*         speedRead();
- */        clearInterval(timeInterval);
-      }
-    }, 1000);
+// Display time left on the button. 
+function appendTime() {
+  timerLeft = timer - past;  
+  console.log({past, remaining: timer - past, timerLeft: timerLeft});
+    
+  // Set the time left to 0 if the timer goes negative
+  if (timerLeft<0){
+      timerLeft = 0;
   }
+  timerButton.textContent = "Time Left: " + timerLeft + "s"
+  setTimerButtonColor()
+}
+
+// Set the Timer button based on the time remaining
+function setTimerButtonColor(){
+    console.log("============================================");
+    console.log("1. timerLeft in setTimerButton", timerLeft);
+
+    if (timerLeft < 10 && timerLeft > 0){
+        timerButton.classList.replace("btn-success","btn-warning");
+    } else if (timerLeft == 0){
+        console.log("2. timerLeft in setTimerButton else if", timerLeft);
+        console.log("============================================");
+        timerButton.classList.replace("btn-warning","btn-danger");
+    }
+}
+
+// Add penality time to the time that has past counter to deduct from timer
+function wrongPenalty() {
+  past += 5;
+}
+
+function startTimer() {
+console.log(" ", )
+  if (timer >= past) {
+    interval = setInterval(function() {
+        if (past % 21 === 0) {
+        past += 5;
+        } else if (timerLeft === 0 ){
+            console.log(" in else if timerLeft", timerLeft)
+            clearInterval(interval)
+        } else {
+            past++
+        }
+        appendTime()
+    }, 1000)
   
-  countdown();
+  } 
+  
+}
 
-// function prepareRead() {
-//   var timeLeft = 5;
-
-//   var timeInterval = setInterval(function() {
-//     timerEl.textContent = timeLeft + " seconds remaining";
-//     timeLeft--;
-
-//     if (timeLeft === 0) {
-//       timerEl.textContent = "";
-//       speedRead();
-//       clearInterval(timeInterval);
-//     }
-//   }, 1000);
-// }
+//startTimer();
 
 
-// function speedRead() {
-//   mainEl.append(bodyEl);
+// function countdown() {
+//     if (status == "Start") {
 
-//   var poemInterval = setInterval(function() {
-//     if (words[i] === undefined) {
-//       clearInterval(poemInterval);
+//         var timeInterval = setInterval(function() {
+//             timerButton.textContent = "Time Left: "+ timeLeft + "s";
+//         timeLeft--;
+//             console.log("Timer left")
+//         if (timeLeft === 0) {
+//             timerButton.textContent = "Time Left: 0";
+//         clearInterval(timeInterval);
+//         }
+//         }, 1000);
+
 //     } else {
-//       mainEl.textContent = words[i];
-//       i++;
-//     }
+//         console.log("In else")
+//         clearInterval(timeInterval);
 
-//   }, millisecondsPerWord);
+//     }
+//     status = "Pause";
+//     console.log("status", status);
+
 // }
 
-// prepareRead();
 
 
+
+
+timerButton.addEventListener("click", startTimer);
+
+// timerButton.addEventListener("click", countdown);
+// pauseButton.addEventListener("click", pauseTimer);
